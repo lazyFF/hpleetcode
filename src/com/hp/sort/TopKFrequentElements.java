2bd0b1fbf8e9da2cc0a9d889bd87d4c85fe1d41d
caihaopeng <caihp54@gmail.com>
2021-05-11 23:40:51 +0800

    自己的解法，使用冒泡排序

--- src/com/hp/sort/TopKFrequentElements.java
+++ src/com/hp/sort/TopKFrequentElements.java
@@ -7,22 +7,42 @@ import java.util.*;
  * @date 14:46 2021/5/10
  **/
 public class TopKFrequentElements {
-    public int[] topKFrequent(int[] nums, int k) {
+    public static int[] topKFrequent(int[] nums, int k) {
         List<Integer> frequentList = new ArrayList<>();
         Map<Integer,Integer> map = new HashMap<>();
         for(int num : nums){
-            if(frequentList.contains(num)){
-                map.put(num,map.get(num) + 1);
-            }else{
-               frequentList.add(num);
-               map.put(num,1);
+//            if(frequentList.contains(num)){
+//                map.put(num,map.get(num) + 1);
+//            }else{
+//                frequentList.add(num);
+//                map.put(num,1);
+//            }
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+        for(int i = 0;i < entryList.size() - 1 ; i++){
+            for(int j = 0; j < entryList.size() - i - 1; j++){
+                if(entryList.get(j).getValue() > entryList.get(j + 1).getValue()){
+                    Map.Entry<Integer,Integer> entry = entryList.get(j+1);
+                    entryList.set(j+1,entryList.get(j));
+                    entryList.set(j,entry);
+                }
             }
         }
 
-        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(entries);
+        int[] arr = new int[k];
+        for(int i = entryList.size() - k,j = 0;i < entryList.size();i++){
+            arr[j] = entryList.get(i).getKey();
+            j++;
+        }
 
+        return arr;
+    }
 
-        return null;
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,2,2,3,3,3,3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(arr, k)));
     }
 }
